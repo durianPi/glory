6d02fd095e705601faac959617a9a682235fcf42
DreamLee <595394202@qq.com>
2018-12-27 23:41:36 +0800

    '新增文件'

---
+++ src/main/java/com/dreamlee/leetcode/number1/Solution.java
@@ -0,0 +1,10 @@
+package com.dreamlee.leetcode.number1;
+
+/**
+ * @author: DreamLee
+ * @date: Created on 23:40 2018/12/27
+ * @description: [describe the function]
+ * @modified:
+ */
+public class Solution {
+}
