a94bebfee730f471a5c8a56cbb43c63441134433
jinxiaotao <825099799@qq.com>
2018-12-28 22:48:42 +0800

    '修改NO1.Solution'

--- src/main/java/com/dreamlee/leetcode/number1/Solution.java
+++ src/main/java/com/dreamlee/leetcode/number1/Solution.java
@@ -3,11 +3,26 @@ package com.dreamlee.leetcode.number1;
 /**
  * @author: DreamLee
  * @date: Created on 23:40 2018/12/27
- * @description: [describe the function]
+ * @description:
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
  * @modified: JinXT002
  */
 public class Solution {
     public static void main(String[] args) {
 
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
     }
 }
