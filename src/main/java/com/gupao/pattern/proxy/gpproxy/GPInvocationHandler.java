a765c45178e7867638d32b280b0ec35c0e50f553
DreamLee <595394202@qq.com>
2019-03-10 22:13:15 +0800

    '咕泡学习代码'

---
+++ src/main/java/com/gupao/pattern/proxy/gpproxy/GPInvocationHandler.java
@@ -0,0 +1,17 @@
+package com.gupao.pattern.proxy.gpproxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author: DreamLee
+ * @date: Created on 21:43 2019/3/10
+ * @description: [describe the function]
+ * @modified:
+ */
+public class GPInvocationHandler {
+
+    public Object invoke(Object proxy, Method method, Object[] args)
+            throws Throwable {
+        return null;
+    }
+}
---
+++ src/main/java/com/gupao/pattern/proxy/staticproxy/Father.java
@@ -0,0 +1,10 @@
+package com.gupao.pattern.proxy.staticproxy;
+
+/**
+ * @author: DreamLee
+ * @date: Created on 21:05 2019/3/10
+ * @description: [describe the function]
+ * @modified:
+ */
+public class Father {
+}
